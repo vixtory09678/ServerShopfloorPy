20acc5cb8c419af99560968cc4cddf65a0fdf81b
roboticsza <robotics.za@gmail.com>
2019-09-28 14:09:19 +0700

    edit url

--- constants.py
+++ constants.py
@@ -3,6 +3,6 @@ CONTROL_PATH_MQTT = "data/maintenance/control_chart"
 PARETO_PATH_MQTT = "data/maintenance/pareto_chart"
 ALARM_PATH_MQTT = "data/maintenance/alarm"
 
-HOST_URL = "192.168.43.1"
+HOST_URL = "192.168.43.70"
 # PROTOCOL
 BUFFER_SIZE = 1024
